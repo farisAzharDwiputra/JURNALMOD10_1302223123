d87c8a0c81fa783f5eb10eb9b6d4d414bba4fec0
Faris Azhar <farisazhar.0241@gmail.com>
2024-04-29 07:24:52 +0700

    Inisiasi Pertama

---
+++ Modul10_1302223123/Controllers/ControllerMahasiswa.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Modul10_1302223123.Controllers
+{
+    public class ControllerMahasiswa : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Modul10_1302223123/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Mahasiswa
+{ 
+		public string Name { get; set; }
+	public string Nim { get; set; }
+	public List <string> Course { get; set; }
+	public int Year { get; set; }
+
+	public Mahasiswa(string Nama,string nim,List<string> course,int year){
+			Name = Nama;
+			Nim = nim;
+			Course = course;
+			Year = year;
+		}
+	}
+}
